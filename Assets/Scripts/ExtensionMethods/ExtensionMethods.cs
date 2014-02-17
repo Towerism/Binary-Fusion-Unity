b87e01ab0c35b5b80da5e11b677ee1b3b1f6b098
Martin <martin.frackerjr@gmail.com>
2014-02-17 00:55:13 -0600

    Player Movement

---
+++ Assets/Scripts/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ExtensionMethods {
+    public static class GameObjectExtensions {
+        public static void AutoCache<T>(this GameObject go, ref T cache, T component) where T : Component {
+            if (cache == null)
+                cache = component;
+            else return;
+        } 
+    }
+}
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using ExtensionMethods;
+
 public class PlayerInput : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    Transform _transform;
 
 	// Use this for initialization
 	void Start () {
-	
+        gameObject.AutoCache<Transform>(ref _transform, transform);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        movement();
 	}
+
+    void movement() {
+        Vector2 hmove = Vector2.right * Input.GetAxis("Horizontal");
+        Vector2 vmove = Vector2.up * Input.GetAxis("Vertical");
+        _transform.Translate(hmove * horizontalSpeed);
+        _transform.Translate(vmove * verticalSpeed);
+    }
 }
