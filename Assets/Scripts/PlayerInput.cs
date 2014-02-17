06c8aeea5b3b23dd5fbeb5dd3f79f31390aa2dcc
Martin <martin.frackerjr@gmail.com>
2014-02-17 02:09:30 -0600

    Player change color animation

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -7,6 +7,9 @@ public class PlayerInput : MonoBehaviour {
 
     public float horizontalSpeed;
     public float verticalSpeed;
+    public tk2dSpriteAnimator animator;
+    int color = 0;
+    bool changingColor = false;
 
     Transform _transform;
 
@@ -18,6 +21,7 @@ public class PlayerInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         movement();
+        colorInput();
 	}
 
     void movement() {
@@ -25,5 +29,22 @@ public class PlayerInput : MonoBehaviour {
         Vector2 vmove = Vector2.up * Input.GetAxis("Vertical");
         _transform.Translate(hmove * horizontalSpeed);
         _transform.Translate(vmove * verticalSpeed);
+    }
+
+    void colorInput() {
+        if (Input.GetKeyDown(KeyCode.X) && !changingColor) {
+            iTween.ScaleTo(gameObject, iTween.Hash("scale", new Vector3(0, 1, 0), "time", .25f, "oncomplete", "switchColor", "easetype", iTween.EaseType.linear));
+            changingColor = true;
+        }
+    }
+
+    void switchColor() {
+        color = color == 0 ? 1 : 0;
+        animator.SetFrame(color);
+        iTween.ScaleTo(gameObject, iTween.Hash("scale", new Vector3(1, 1, 0), "time", .25f, "easetype", iTween.EaseType.linear, "oncomplete", "setChangingColorFalse"));
+    }
+
+    void setChangingColorFalse() {
+        changingColor = false;
     }
 }
